9f1dc01645b0a216fd2b71df4d589d819457348b
Vedank Naik <vedankn01@gmail.com>
2022-06-28 18:03:53 +0530

    Fetch playlists

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   HomeIcon,
   SearchIcon,
@@ -8,20 +8,28 @@ import {
   PlusCircleIcon,
 } from '@heroicons/react/outline'
 import { signOut, useSession } from 'next-auth/react'
+import useSpotify from '../hooks/useSpotify'
 
 function Sidebar() {
+  const spotifyApi = useSpotify()
   const { data: session, status } = useSession()
+  const [playlists, setPlaylists] = useState([])
 
-  console.log('~ session', session)
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((res) => {
+        setPlaylists(res.body.items)
+      })
+    }
+  }, [session, spotifyApi])
 
   return (
-    <div className="border-r border-gray-900 p-5 text-sm text-gray-500">
+    <div className="h-screen overflow-y-scroll border-r border-gray-900 p-5 text-sm text-gray-500 scrollbar-hide">
       <div className="space-y-4">
         <button
           className="flex items-center space-x-2 hover:text-white"
           onClick={() => signOut()}
         >
-          <HomeIcon className="h-5 w-5" />
           <p>Logout</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
@@ -54,10 +62,10 @@ function Sidebar() {
 
         <hr className="border-t-[0.1px] border-gray-900" />
 
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
+        {playlists.map((playlist) => (
+        <p key={playlist.id} className="cursor-pointer hover:text-white">{playlist.name}</p>
+        ))}
+
       </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
 import Sidebar from '../components/Sidebar'
+import Center from '../components/Center'
 
 const Home: NextPage = () => {
   return (
     <div className="h-screen overflow-hidden bg-black">
-      <main>
+      <main className="flex">
         <Sidebar />
+        <Center />
         {/* center */}
       </main>
       <div>{/* player */}</div>
