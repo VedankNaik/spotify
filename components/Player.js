9c39880ffef54bc588dc5adc0de0f5c5aef85f67
Vedank Naik <vedankn01@gmail.com>
2022-08-01 14:36:10 +0530

    add volume controls

--- components/Player.js
+++ components/Player.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import useSpotify from '../hooks/useSpotify'
 import useSongInfo from '../hooks/useSongInfo'
 import { useSession, signOut } from 'next-auth/react'
@@ -11,7 +11,10 @@ import {
   ReplyIcon,
   RewindIcon,
   SwitchHorizontalIcon,
+  VolumeOffIcon,
+  VolumeUpIcon,
 } from '@heroicons/react/solid'
+import { debounce } from 'lodash'
 
 function Player() {
   const spotifyApi = useSpotify()
@@ -54,13 +57,26 @@ function Player() {
     })
   }
 
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debounceAdjustVolume(volume)
+    }
+  }, [volume])
+
+  const debounceAdjustVolume = useCallback(
+    debounce((volume) => {
+      spotifyApi.setVolume(volume).catch((err) => {})
+    }, 500),
+    []
+  )
+
   return (
     <div className="grid h-24 grid-cols-3 bg-gradient-to-b from-black to-gray-900 px-2 text-xs text-white md:px-8 md:text-base">
       <div className="flex items-center space-x-4">
         <img
           className="hidden h-10 w-10 md:inline"
           src={songInfo?.album.images?.[0]?.url}
-          alt="Song Image"
+          alt=""
         />
         <div>
           <h3>{songInfo?.name}</h3>
@@ -70,7 +86,7 @@ function Player() {
       <div className="flex items-center justify-evenly">
         <SwitchHorizontalIcon className="button" />
         <RewindIcon
-          // onClick={() => spotifyApi.skipToPrevious()}
+          onClick={() => spotifyApi.skipToPrevious()}
           className="button"
         />
         {isplaying ? (
@@ -79,10 +95,29 @@ function Player() {
           <PlayIcon onClick={handlePlayPause} className="button h-10 w-10" />
         )}
         <FastForwardIcon
-          // onClick={() => spotifyApi.skipToNext()}
+          onClick={() => spotifyApi.skipToNext()}
           className="button"
         />
         <ReplyIcon className="button" />
+      </div>
+
+      <div className="flex items-center justify-end space-x-3 pr-5 md:space-x-4">
+        <VolumeOffIcon
+          onClick={() => volume > 0 && setVolume(volume - 10)}
+          className="button"
+        />
+        <input
+          className="w-14 md:w-28"
+          type="range"
+          value={volume}
+          onChange={(e) => setVolume(Number(e.target.value))}
+          min="0"
+          max="100"
+        />
+        <VolumeUpIcon
+          onClick={() => volume < 100 && setVolume(volume + 10)}
+          className="button"
+        />
       </div>
     </div>
   )
